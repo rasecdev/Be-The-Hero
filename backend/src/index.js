7fee343ca5511dee61c30795eaf3d89987bf32a0
Anderson Jose Cesar Pereira <ajpereira@latam.stefanini.com>
2020-04-16 02:01:28 -0300

    Subindo o Back e o Front

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.get('/', (require, Response) => {
+    return Response.send('Hello world!');
+});
+
+app.listen(3333);
